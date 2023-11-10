95ec3bac0ef27070dd2ddfcca8e09a495550076a
Andrews0897 <ryanfoo834@gmail.com>
2023-11-10 10:56:42 +0530

    First Commit

---
+++ src/DEMOREPO/DEMOREPO.java
@@ -0,0 +1,18 @@
+package DEMOREPO;
+
+public class DEMOREPO {
+	
+	public static void main(String[] args) {
+		
+		int number = 10;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+	}
+
+}
